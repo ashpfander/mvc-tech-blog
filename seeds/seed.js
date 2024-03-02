c29276e2f75816969280ae8a2fde0240e7a7e38e
Ashleigh Pfander <ashleighharvey@Ashleighs-MacBook-Pro.local>
2024-03-02 17:48:25 -0500

    updated comment and post seeds, added code for seeding the database

---
+++ seeds/seed.js
@@ -0,0 +1,28 @@
+// Imports the sequelize connection in the config folder
+const sequelize = require('../config/connection');
+// Grabs the models from the models folder
+const { User, Post, Comment } = require('../models');
+
+// Creating variables for each of the seeds data
+const userData = require('./userData.json');
+const postData = require('./projectData.json');
+const commentData = require('./commentData.json');
+
+// Function for seeding the database
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  // Bulk create the user data from json file
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  // Bulk create the posts and comments from the json files
+  await Post.bulkCreate(postData);
+  await Comment.bulkCreate(commentData);
+
+  process.exit(0);
+};
+
+seedDatabase();
